3184a426ae82effb27f15288382e4fbda2598d8e
subham-rakshit <subhamrakshit667@gmail.com>
2024-11-04 09:19:07 +0530

    Create Next.js 15 App with flowbite and flowbite-react

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import { Button } from "flowbite-react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Button color="blue">Click Me</Button>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}", // Flowbite React components
   ],
   theme: {
     extend: {
@@ -13,5 +14,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
