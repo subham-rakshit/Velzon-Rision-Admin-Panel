eba63ac300ace588ecd3619108b61233eca3a239
subham-rakshit <subhamrakshit667@gmail.com>
2024-11-04 09:32:59 +0530

    Create authentication routes (eg. login, register, forget-password)

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,16 @@
+"use client";
+
 import { Button } from "flowbite-react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/login");
+  }, [router]);
+
   return (
     <div>
       <h1>Home Page</h1>
